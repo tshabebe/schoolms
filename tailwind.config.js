b17cff4458ed54681d04a2bc8cc9e2f917113303
teshe <teshomeabebe114@gmail.com>
2024-05-29 12:34:24 +0000

    sytle: add custom colors and sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/theme";
+import { primary, secondaryDark, secondaryLight } from "./utils/colors";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -16,5 +17,37 @@ module.exports = {
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
-}
+  plugins: [
+    nextui({
+      prefix: "noodle",
+      layout: {
+        radius: {
+          small: "4px",
+          medium: "8px",
+          large: "12px",
+        },
+      },
+      themes: {
+        light: {
+          colors: {
+            background: "#ffffff",
+            foreground: "#000000",
+            primary: {
+              ...primary,
+              foreground: "#ffffff",
+            },
+            secondary: secondaryLight,
+          },
+        },
+        dark: {
+          colors: {
+            background: "#000000",
+            foreground: "#ffffff",
+            primary,
+            secondary: secondaryDark,
+          },
+        },
+      },
+    }),
+  ],
+};
---
+++ utils/colors.ts
@@ -0,0 +1,46 @@
+export const primary = {
+  foreground: "#000000",
+  DEFAULT: "#F9617B",
+  50: "#FEF0F3",
+  100: "#FEE2E6",
+  200: "#FDBFC9",
+  300: "#FBA2B1",
+  400: "#FA7F94",
+  500: "#F9617B",
+  600: "#F61D41",
+  700: "#C90828",
+  800: "#85051A",
+  900: "#45030E",
+};
+
+export const secondaryDark = {
+  background: "#FFFFFF",
+  foreground: "#000000",
+  DEFAULT: "#FFFFFF",
+  50: "#FFFFFF",
+  100: "#FFFFFF",
+  200: "#FFFFFF",
+  300: "#FFFFFF",
+  400: "#FFFFFF",
+  500: "#FFFFFF",
+  600: "#E3E3E3",
+  700: "#C7C7C7",
+  800: "#ABABAB",
+  900: "#8F8F8F",
+};
+
+export const secondaryLight = {
+  background: "#000000",
+  foreground: "#FFFFFF",
+  DEFAULT: "#000000",
+  50: "#5C5C5C",
+  100: "#525252",
+  200: "#3D3D3D",
+  300: "#292929",
+  400: "#141414",
+  500: "#000000",
+  600: "#000000",
+  700: "#000000",
+  800: "#000000",
+  900: "#000000",
+};
